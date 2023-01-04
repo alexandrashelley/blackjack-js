82bcc48f47f55b570423476b2e2451b7f10cf3dc
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-04 16:12:41 +0000

    createDeck method creates a new deck with 52 cards pass

--- spec/Deck.test.js
+++ spec/Deck.test.js
@@ -1,14 +1,16 @@
 const Deck = require("../lib/Deck");
+const Card = require("../lib/Card")
 
 describe("the Deck object", () => {
-
-  it("A deck is initialised with with an array", () => {
-    const deck = new Deck();
-    expect(deck.deck).toEqual([]);
-  });
 
   it("A deck is initialised with an array", () => {
     const deck = new Deck();
     expect(deck.deck).toEqual([]);
+  });
+
+  it("the createDeck method populates the deck with 52 cards", () => {
+    const deck = new Deck();
+    deck.createDeck()
+    expect(deck.deck.length).toEqual(52)
   });
 });
