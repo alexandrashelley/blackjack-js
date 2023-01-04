97cba02e517da93af4a7fad9a14f7a9c3073c512
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-04 15:41:58 +0000

    file saved

--- spec/Card.test.js
+++ spec/Card.test.js
@@ -1,33 +1,15 @@
 const Card = require("../lib/Card");
 
 describe("the Card object", () => {
-  // it("the getRandom method retrieves a random number", () => {
-  //   const card = new Card();
-  //   const result = card.getRandom(10);
-  //   expect(result).toBeGreaterThanOrEqual(0)
-  //   expect(result).toBeLessThanOrEqual(10);
-  // });
-
   it("gets the card's value", () => {
     const card = new Card();
-    const result = card.getValues();
-    expect(result).toBe(card.values)
+    const result = card.getValue();
+    expect(result).toBe(card.value)
   });
 
   it("gets the card's suit", () => {
     const card = new Card();
-    const result = card.getValues();
-    expect(result).toBe(card.values)
+    const result = card.getSuit();
+    expect(result).toBe(card.suit)
   });
 });
-
-  // it("the getCard method always returns a suit", () => {
-  //   const card = new Card;
-  //   expect(card.getCard()).toMatch(/(Diamonds|Hearts|Spades|Clubs)/)
-  // });
-
-  // it("the getCard method always returns a value", () => {
-  //   const card = new Card;
-  //   expect(card.getCard()).toMatch(/(Ace|1|2|3|4|5|6|7|8|9|10|Jack|Queen|King)/)
-  // });
-
