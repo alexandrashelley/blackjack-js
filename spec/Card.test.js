8631d0fc1397be4ffc0ec6fe329fccd9861a8f40
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-04 15:40:04 +0000

    card stripped of hard values and suits - moved to deck (a card's values are determined by the game/deck

--- spec/Card.test.js
+++ spec/Card.test.js
@@ -1,22 +1,33 @@
-require('jest-fetch-mock').enableFetchMocks()
 const Card = require("../lib/Card");
 
 describe("the Card object", () => {
+  // it("the getRandom method retrieves a random number", () => {
+  //   const card = new Card();
+  //   const result = card.getRandom(10);
+  //   expect(result).toBeGreaterThanOrEqual(0)
+  //   expect(result).toBeLessThanOrEqual(10);
+  // });
 
-  it("the getRandom method retrieves a random number", () => {
+  it("gets the card's value", () => {
     const card = new Card();
-    const result = card.getRandom(10);
-    expect(result).toBeGreaterThanOrEqual(0)
-    expect(result).toBeLessThanOrEqual(10);
+    const result = card.getValues();
+    expect(result).toBe(card.values)
   });
 
-  it("the getCard method always returns a suit", () => {
-    const card = new Card;
-    expect(card.getCard()).toMatch(/(Diamonds|Hearts|Spades|Clubs)/)
-  });
-
-  it("the getCard method always returns a value", () => {
-    const card = new Card;
-    expect(card.getCard()).toMatch(/(Ace|1|2|3|4|5|6|7|8|9|10|Jack|Queen|King)/)
+  it("gets the card's suit", () => {
+    const card = new Card();
+    const result = card.getValues();
+    expect(result).toBe(card.values)
   });
 });
+
+  // it("the getCard method always returns a suit", () => {
+  //   const card = new Card;
+  //   expect(card.getCard()).toMatch(/(Diamonds|Hearts|Spades|Clubs)/)
+  // });
+
+  // it("the getCard method always returns a value", () => {
+  //   const card = new Card;
+  //   expect(card.getCard()).toMatch(/(Ace|1|2|3|4|5|6|7|8|9|10|Jack|Queen|King)/)
+  // });
+
