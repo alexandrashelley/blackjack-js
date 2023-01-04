f5d20dc35f5fc84ea6ca56ee14a029066c2120cf
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-04 16:31:06 +0000

    shuffle function test pass

--- spec/Deck.test.js
+++ spec/Deck.test.js
@@ -1,8 +1,7 @@
 const Deck = require("../lib/Deck");
-const Card = require("../lib/Card")
+const Card = require("../lib/Card");
 
 describe("the Deck object", () => {
-
   it("A deck is initialised with an array", () => {
     const deck = new Deck();
     expect(deck.deck).toEqual([]);
@@ -10,7 +9,14 @@ describe("the Deck object", () => {
 
   it("the createDeck method populates the deck with 52 cards", () => {
     const deck = new Deck();
-    deck.createDeck()
-    expect(deck.deck.length).toEqual(52)
+    deck.createDeck();
+    expect(deck.deck.length).toEqual(52);
+  });
+
+  it("the shuffleCards method shuffles the deck", () => {
+    const deck = new Deck();
+    deck.deck = [1, 2];
+    deck.shuffle();
+    expect(deck.deck).toEqual([2, 1]);
   });
 });
