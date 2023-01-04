904520e3c82685dd9a5c71ab19b3123fca0655e2
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-04 09:57:17 +0000

    deck initialises with an array test pass

---
+++ spec/Deck.test.js
@@ -0,0 +1,9 @@
+const Deck = require("../lib/Deck");
+
+describe("the Deck object", () => {
+
+  it("A deck is initialised with with an array", () => {
+    const deck = new Deck();
+    expect(deck.deck).toEqual([]);
+  });
+});
