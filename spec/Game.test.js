cd52436dfa7ab42988af515322f21364702f6ff5
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-05 13:15:03 +0000

    game class can deal a hand and card tests passing

--- spec/Game.test.js
+++ spec/Game.test.js
@@ -1,14 +1,22 @@
 const Game = require("../lib/Game");
 
 describe("the Game object", () => {
-  it("initialising a new Game makes an empty hand", () => {
+  it("initialising a new Game deals a hand with 2 cards", () => {
     const game = new Game();
-    expect(game.hand.length).toBe(0);
+    game.dealHand();
+    expect(game.hand.length).toBe(2);
   });
 
   it("the dealHand function deals a hand 2 cards", () => {
     const game = new Game();
     game.dealHand();
     expect(game.hand.length).toBe(2);
+  });
+
+  it("the dealCard function deals a hand 1 card", () => {
+    const game = new Game();
+    game.dealHand();
+    game.dealCard();
+    expect(game.hand.length).toBe(3);
   });
 });
