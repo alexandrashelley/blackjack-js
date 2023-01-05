2b6e92bd7f8c5d99ef9f940cc3802dddc0f2835d
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-05 12:15:09 +0000

    a new deck is populated with 52 cards, createDeck absorbed in constructor

--- spec/Deck.test.js
+++ spec/Deck.test.js
@@ -2,14 +2,8 @@ const Deck = require("../lib/Deck");
 const Card = require("../lib/Card");
 
 describe("the Deck object", () => {
-  it("A deck is initialised with an array", () => {
+  it("a new deck is populated with 52 cards", () => {
     const deck = new Deck();
-    expect(deck.deck).toEqual([]);
-  });
-
-  it("the createDeck method populates the deck with 52 cards", () => {
-    const deck = new Deck();
-    deck.createDeck();
     expect(deck.deck.length).toEqual(52);
   });
 
