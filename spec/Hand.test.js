2a0751c176378ac42e69263f117df79e8d80dc33
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-05 16:12:45 +0000

    hand can now get value of cards in hand

--- spec/Hand.test.js
+++ spec/Hand.test.js
@@ -3,8 +3,21 @@ const Hand = require("../lib/Hand");
 describe("the Hand object", () => {
   it("the showHand function shows the player the cards in their hand", () => {
     const hand = new Hand();
-    hand.hand = [{"suit": "Diamonds", "value": "Ace"}, {"suit": "Diamonds", "value": "10"}]
+    hand.hand = [
+      { suit: "Diamonds", value: "Ace" },
+      { suit: "Diamonds", value: "10" },
+    ];
     const result = hand.showHand();
-    expect(result).toBe("Ace of Diamonds, 10 of Diamonds") 
+    expect(result).toBe("Ace of Diamonds, 10 of Diamonds");
+  });
+
+  it("gets the value of the current hand", () => {
+    const hand = new Hand();
+    hand.hand = [
+      { suit: "Diamonds", value: "9" },
+      { suit: "Diamonds", value: "10" },
+    ];
+    const result = hand.getHandValue();
+    expect(result).toBe(19);
   });
 });
