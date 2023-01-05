7591e6091407edca6be07775f62a36b59b155753
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-05 16:10:39 +0000

    deck handles responsibility of pulling top card

--- spec/Deck.test.js
+++ spec/Deck.test.js
@@ -2,15 +2,22 @@ const Deck = require("../lib/Deck");
 const Card = require("../lib/Card");
 
 describe("the Deck object", () => {
-  it("a new deck is populated with 52 cards", () => {
+  it("the createDeck method populates the deck with 52 cards", () => {
     const deck = new Deck();
     expect(deck.deck.length).toEqual(52);
   });
 
-  it("the shuffleCards method shuffles the deck", () => {
+  xit("the shuffleCards function shuffles the deck", () => {
     const deck = new Deck();
     deck.deck = [1, 2];
     deck.shuffle();
     expect(deck.deck).toEqual([2, 1]);
+  });
+
+  it("the getTopCard function gets top card from the deck", () => {
+    const deck = new Deck();
+    console.log(deck.deck)
+    const result = deck.getTopCard();
+    expect(result).toEqual([{"face": "2", "suit": "Diamonds", "value": 2}])
   });
 });
