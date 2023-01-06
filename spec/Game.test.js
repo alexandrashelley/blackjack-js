bd1cb541e007b663f18d19e109f7e7020dd1def8
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-06 13:45:22 +0000

    rejigging classes. game class is to carry out more basic functionality, new class will oversee input and prompts

--- spec/Game.test.js
+++ spec/Game.test.js
@@ -1,48 +1,43 @@
 const readlineSync = require("readline-sync");
-const Deck = require("../lib/Deck");
 const Game = require("../lib/Game");
 
 describe("the Game object", () => {
   it("initialising a new Game deals a hand with 2 cards", () => {
     const game = new Game();
     game.dealHand();
-    expect(game.hand.length).toBe(2);
+    expect(game.hand.hand.length).toBe(2);
   });
 
   it("the dealHand function deals a hand 2 cards", () => {
     const game = new Game();
     game.dealHand();
-    expect(game.hand.length).toBe(2);
+    expect(game.hand.hand.length).toBe(2);
   });
 
   it("the dealCard function deals a hand 1 card", () => {
     const game = new Game();
     game.dealHand();
     game.dealCard();
-    expect(game.hand.length).toBe(3);
+    expect(game.hand.hand.length).toBe(3);
   });
 
   it("the dealCard function deals a hand 1 card", () => {
     const game = new Game();
     game.dealHand();
     game.dealCard();
-    expect(game.hand.length).toBe(3);
+    expect(game.hand.hand.length).toBe(3);
   });
 
-  // I need a way of mocking the user input... for now tests pass when
-  // correct answer is inputted for expectation
+  it("startGame begins a new game", () => {
+    const game = new Game();
+    game.startGame();
+    expect(game.hand.hand.length).toBe(2);
+  });
 
   it("if the player chooses hit, another card is added to their hand", () => {
     const game = new Game();
     game.dealHand();
-    game.play();
-    expect(game.hand.length).toBe(3);
-  });
-
-  it("if the player chooses stand, the hand stays at a total of 2 cards", () => {
-    const game = new Game();
-    game.dealHand();
-    game.play();
-    expect(game.hand.length).toBe(2);
+    game.hit();
+    expect(game.hand.hand.length).toBe(3);
   });
 });
