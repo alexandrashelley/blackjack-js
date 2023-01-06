f588be7d821dbb54002b16101fc81fa4ea48898b
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-06 15:14:16 +0000

    calculateHandValue correctly assigns ace values

--- spec/Hand.test.js
+++ spec/Hand.test.js
@@ -11,7 +11,7 @@ describe("the Hand object", () => {
     expect(result).toBe("Ace of Diamonds, 10 of Diamonds");
   });
 
-  it("gets the value of the current hand", () => {
+  xit("gets the value of the current hand", () => {
     const hand = new Hand();
     hand.hand = [
       { suit: "Diamonds", value: "9" },
@@ -23,10 +23,61 @@ describe("the Hand object", () => {
 
   it("addNCardsToHand pushes cards into hand", () => {
     const hand = new Hand();
-    const card = [
-      { suit: "Diamonds", value: "9" },
-    ];
+    const card = [{ suit: "Diamonds", value: "9" }];
     hand.addNCardsToHand(card);
-    expect(hand.hand).toEqual([{"suit": "Diamonds", "value": "9"}]);
+    expect(hand.hand).toEqual([{ suit: "Diamonds", value: "9" }]);
+  });
+
+  it("addNCardsToHand pushes cards into hand", () => {
+    const hand = new Hand();
+    const card = [{ suit: "Diamonds", value: "9" }];
+    hand.addNCardsToHand(card);
+    expect(hand.hand).toEqual([{ suit: "Diamonds", value: "9" }]);
+  });
+
+  it("calculateHandValue returns 21 for a hand containing a king and an ace", () => {
+    const hand = new Hand();
+    const cards = [
+      { suit: "Diamonds", value: "11" },
+      { suit: "Diamonds", value: "10" },
+    ];
+    hand.addNCardsToHand(cards);
+    const result = hand.calculateHandValue();
+    expect(result).toBe(21);
+  });
+
+  it("calculateHandValue returns 21 for a hand containing a nine and two aces", () => {
+    const hand = new Hand();
+    const cards = [
+      { suit: "Diamonds", value: "9" },
+      { suit: "Diamonds", value: "11" },
+      { suit: "Hearts", value: "11" },
+    ];
+    hand.addNCardsToHand(cards);
+    const result = hand.calculateHandValue();
+    expect(result).toBe(21);
+  });
+
+  it("calculateHandValue returns 21 for a hand containing a king, queen and an ace", () => {
+    const hand = new Hand();
+    const cards = [
+      { suit: "Diamonds", value: "10" },
+      { suit: "Diamonds", value: "10" },
+      { suit: "Hearts", value: "11" },
+    ];
+    hand.addNCardsToHand(cards);
+    const result = hand.calculateHandValue();
+    expect(result).toBe(21);
+  });
+
+  it("calculateHandValue returns 12 for a hand containing two aces", () => {
+    const hand = new Hand();
+    const cards = [
+      { suit: "Diamonds", value: "11" },
+      { suit: "Hearts", value: "11" },
+    ];
+    hand.addNCardsToHand(cards);
+    const result = hand.calculateHandValue();
+    expect(result).toBe(12);
   });
 });
