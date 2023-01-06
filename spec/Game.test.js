a683709f370921684947e4d6280f876cd8f60065
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-06 13:38:47 +0000

    hand has function for adding card to hand

--- spec/Game.test.js
+++ spec/Game.test.js
@@ -1,3 +1,4 @@
+const readlineSync = require("readline-sync");
 const Deck = require("../lib/Deck");
 const Game = require("../lib/Game");
 
@@ -19,5 +20,29 @@ describe("the Game object", () => {
     game.dealHand();
     game.dealCard();
     expect(game.hand.length).toBe(3);
+  });
+
+  it("the dealCard function deals a hand 1 card", () => {
+    const game = new Game();
+    game.dealHand();
+    game.dealCard();
+    expect(game.hand.length).toBe(3);
+  });
+
+  // I need a way of mocking the user input... for now tests pass when
+  // correct answer is inputted for expectation
+
+  it("if the player chooses hit, another card is added to their hand", () => {
+    const game = new Game();
+    game.dealHand();
+    game.play();
+    expect(game.hand.length).toBe(3);
+  });
+
+  it("if the player chooses stand, the hand stays at a total of 2 cards", () => {
+    const game = new Game();
+    game.dealHand();
+    game.play();
+    expect(game.hand.length).toBe(2);
   });
 });
