bfbb3ddb0f1352896f7235cb3b17f40867570848
Alexandra Shelley <alexbshelley@gmail.com>
2023-01-08 15:24:36 +0000

    showhand function updated to show face (not value) and join function reserved for terminal

--- spec/Hand.test.js
+++ spec/Hand.test.js
@@ -4,21 +4,11 @@ describe("the Hand object", () => {
   it("the showHand function shows the player the cards in their hand", () => {
     const hand = new Hand();
     hand.hand = [
-      { suit: "Diamonds", value: "Ace" },
-      { suit: "Diamonds", value: "10" },
+      { suit: "Diamonds", face: "Ace" },
+      { suit: "Diamonds", face: "10" },
     ];
     const result = hand.showHand();
-    expect(result).toBe("Ace of Diamonds, 10 of Diamonds");
-  });
-
-  xit("gets the value of the current hand", () => {
-    const hand = new Hand();
-    hand.hand = [
-      { suit: "Diamonds", value: "9" },
-      { suit: "Diamonds", value: "10" },
-    ];
-    const result = hand.getHandValue();
-    expect(result).toBe(19);
+    expect(result).toEqual(["Ace of Diamonds", "10 of Diamonds"]);
   });
 
   it("addNCardsToHand pushes cards into hand", () => {
